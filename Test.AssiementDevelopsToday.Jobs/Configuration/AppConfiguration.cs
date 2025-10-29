e526a4d88616ed620cd8eec64900626ebc8e4815
andrii-kozmenchuk <andrii-kozmenchuk@vertexcode.dev>
2025-10-29 13:21:40 +0200

    add env variable for AppConfiguration

--- Test.AssiementDevelopsToday.Jobs/Configuration/AppConfiguration.cs
+++ Test.AssiementDevelopsToday.Jobs/Configuration/AppConfiguration.cs
@@ -2,26 +2,15 @@ namespace Test.AssiementDevelopsToday.Jobs.Configuration;
 
 public static class AppConfiguration
 {
-    public static string CsvInputPath = @"C:\Users\225588\RiderProjects\Test.AssiementDevelopsToday.Jobs\Test.AssiementDevelopsToday.Jobs\Files\sample-cab-data (1).csv";
-    public static string DuplicatesOutputPath = @"C:\Users\225588\RiderProjects\Test.AssiementDevelopsToday.Jobs\Test.AssiementDevelopsToday.Jobs\Files\duplicates.csv";
+    public static string CsvInputPath = Environment.GetEnvironmentVariable("CSV_INPUT_PATH") ??
+                                        @"C:\Users\225588\RiderProjects\Test.AssiementDevelopsToday.Jobs\Test.AssiementDevelopsToday.Jobs\Files\sample-cab-data (1).csv";
 
-    public static string ConnectionString { get; set; } =
-        "Server=localhost;Database=TaxiDb;Trusted_Connection=True;TrustServerCertificate=True;";
-    
-    public static string TargetTable { get; set; } = "dbo.TripRecords";
+    public static string DuplicatesOutputPath = Environment.GetEnvironmentVariable("CSV_OUTPUT_OUTPUT_PATH") ??
+                                                @"C:\Users\225588\RiderProjects\Test.AssiementDevelopsToday.Jobs\Test.AssiementDevelopsToday.Jobs\Files\duplicates.csv";
 
+    public static string ConnectionString { get; set; } = Environment.GetEnvironmentVariable("CONNECTION_STRING") ??
+                                                          "Server=localhost;Database=TaxiDb;Trusted_Connection=True;TrustServerCertificate=True;";
 
-    static AppConfiguration()
-    {
-        // TrySetValueFromEnvironment()
-    }
-    
-    private static void TrySetValueFromEnvironment(string environmentKey, ref string value)
-    {
-        var environmentValue = Environment.GetEnvironmentVariable(environmentKey);
-        if(string.IsNullOrEmpty(environmentValue))
-            return;
-
-        value = environmentValue;
-    }
+    public static string TargetTable { get; set; } =
+        Environment.GetEnvironmentVariable("TABLE_NAME") ?? "dbo.TripRecords";
 }
